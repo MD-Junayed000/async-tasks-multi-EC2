908200ebb04276635db0778d9d7054dcfe6d5252
Muhammad Junayed <u2008023@student.cuet.ac.bd>
2025-06-05 08:41:42 +0600

    Update celeryconfig.py

--- app/celeryconfig.py
+++ app/celeryconfig.py
@@ -1,13 +1,13 @@
-
+'''
 ## for a single instances
 broker_url = 'amqp://guest:guest@rabbitmq:5672//'
 result_backend = 'redis://redis:6379/0'
-
 '''
+
 # For Multiple Instances
 broker_url = 'amqp://guest:guest@<BROKER_IP>:5672//'
 result_backend = 'redis://<REDIS_IP>:6379/0'
-'''
+
 task_serializer = 'json'
 result_serializer = 'json'
 accept_content = ['json']
